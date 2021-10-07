35982e921898163611461d3a39f3aa11b2b13d42
dice <ds_work_prog@outlook.jp>
2021-10-07 23:36:52 +0900

    サインイン

--- components/Layout.js
+++ components/Layout.js
@@ -27,11 +27,16 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
                                     Contact
                                 </a>
                             </Link>
+                            <Link href="/signin">
+                                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                                    Sign In
+                                </a>
+                            </Link>
                         </div>
                     </div>
                 </nav>
             </header>
-            <main className="flex flex-1 justify-center items-center flex-col w-full">
+            <main className="flex flex-1 justify-center items-center flex-col w-full bg-gray-50">
                 {children}
             </main>
             <footer className="w-full h-12 flex justify-center items-center border-t">
---
+++ pages/signin.js
@@ -0,0 +1,79 @@
+import Layout from "../components/Layout"
+import Image from "next/image"
+
+export default function Signin () {
+    return (
+        <Layout title="Contact">
+            <div className="flex">
+                <div className="flex-auto px-5">
+                    <form className="bg-white text-center shadow-xl p-8 w-96 rounded border">
+                        <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            ユーザー名
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="ユーザー名" />
+                        </div>
+                        <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            メールアドレス
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="email" />
+                        </div>
+                        <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            パスワード
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                        <p className="text-red-500 text-xs italic hidden">Please choose a password.</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                            Forgot Password?
+                        </a>
+                        </div>
+                        <p className="text-center text-gray-500 text-xs">
+                            {/*&copy;2020 Acme Corp. All rights reserved.*/}
+                        </p>
+                    </form>
+                </div>
+                <div className="flex-auto px-5">
+                    <div className="bg-white text-center shadow-xl p-8 w-96 rounded border">
+                        <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            ユーザー名
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="ユーザー名" />
+                        </div>
+                        <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            メールアドレス
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="email" />
+                        </div>
+                        <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            パスワード
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                        <p className="text-red-500 text-xs italic hidden">Please choose a password.</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                            Forgot Password?
+                        </a>
+                        </div>
+                        <p className="text-center text-gray-500 text-xs">
+                            {/*&copy;2020 Acme Corp. All rights reserved.*/}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
