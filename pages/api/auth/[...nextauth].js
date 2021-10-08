6af908b51f2fc8e8473d2b412bd1ef33902b784e
dice <ds_work_prog@outlook.jp>
2021-10-08 22:20:43 +0900

    test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,28 +3,13 @@ import Providers from 'next-auth/providers'
 
 // NextAuth.js関数に渡すオプション
 const options = {
-  providers: [
-    Providers.Credentials({
-      // NextAuthの認証関数。credentialsにログイン情報が格納される。
-      authorize: async credentials => {
-        if (
-          // ログインID・パスワードは環境変数にて設定する。
-          credentials.login === process.env.NEXT_PUBLIC_LOGIN_ID &&
-          credentials.password === process.env.NEXT_PUBLIC_PASSWORD
-        ) {
-          // ログイン成功後ユーザー情報を返却する。値はsessionに格納される。
-          return Promise.resolve({ name: 'admin' })
-        } else {
-          // ログイン失敗後認証を拒否し、エラーメッセージを返却する。
-          return Promise.resolve(null)
-        }
-      },
-    }),
-    Providers.GitHub({
-        clientId: process.env.GITHUB_ID ?? "",
-        clientSecret: process.env.GITHUB_SECRET ?? "",
-      }),
-  ],
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
+        // ...add more providers here
+        ],
   // ログインページを指定する。今回はトップページのため'/'を指定。
   pages: {
     signIn: '/',
