e11233c710d0839237531e9d68f40a3f8784664b
dice <ds_work_prog@outlook.jp>
2021-10-08 22:08:20 +0900

    nextAuth

--- pages/signin.js
+++ pages/signin.js
@@ -42,10 +42,9 @@ export default function Signin () {
                 <div className="flex-auto px-5">
                     <div className="bg-white text-center shadow-xl p-8 w-96 rounded border">
                         <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                            ユーザー名
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="ユーザー名" />
+                            <button onClick={() => signIn()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                                GitHub
+                            </button>
                         </div>
                         <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
