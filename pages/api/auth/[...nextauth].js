495661aa3ba1bbce9db823053372aa2e556546d9
dice <ds_work_prog@outlook.jp>
2021-10-09 20:35:00 +0900

    NextAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,6 +18,20 @@ const options = {
         })
         // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            return true
+        },
+        async redirect({ url, baseUrl }) {
+            return baseUrl
+        },
+        async session({ session, user, token }) {
+            return session
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            return token
+        }
+    }
   // ログインページを指定する。今回はトップページのため'/'を指定。
  /* pages: {
     signIn: '/signin',
