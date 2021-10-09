d4eb88207a873606d85463c789c91330618a63f7
dice <ds_work_prog@outlook.jp>
2021-10-09 21:58:28 +0900

    aaa

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,7 +23,7 @@ const options = {
             return true
         },
         async redirect({ url, baseUrl }) {
-            return url+"/signin"
+            return baseUrl
         },
         async session({ session, user, token }) {
             return session
