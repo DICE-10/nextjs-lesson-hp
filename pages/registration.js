ff816cabb7c26907652d46f85ecbd7a2721fc7d8
dice <ds_work_prog@outlook.jp>
2021-10-10 16:00:21 +0900

    regist

--- pages/registration.js
+++ pages/registration.js
@@ -7,8 +7,8 @@ export default function Registration({ Providers }) {
     return (
         <Layout title="会員登録">
             <form className="w-full max-w-lg">
-                <div className="flex flex-wrap -mx-2 mb-8">
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Name"/>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <input className="appearance-none block w-full text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Name"/>
                 </div>
             </form>
         </Layout>
