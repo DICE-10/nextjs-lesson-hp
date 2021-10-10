f57477333f98332ec268ec37fde66f03ee0af3ac
dice <ds_work_prog@outlook.jp>
2021-10-10 21:55:38 +0900

    regist

--- pages/registration.js
+++ pages/registration.js
@@ -6,7 +6,6 @@ export default function Registration({ Providers }) {
     const [session] = useSession()
     return (
         <Layout title="会員登録">
-            <div className="flex justify-center items-center h-screen mx-auto bg-gray-100">
             <form action="#" className="w-full md:w-3/4 lg:w-3/6 p-4">
                 <div className="p-3">
                     <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Name" required/>
@@ -26,7 +25,6 @@ export default function Registration({ Providers }) {
                     </button>
                 </div>
                 </form>
-            </div>
         </Layout>
     )
 }
