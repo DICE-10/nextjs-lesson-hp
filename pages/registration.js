c51c26b89da0c31e8196798c263ea13c8713538c
dice <ds_work_prog@outlook.jp>
2021-10-10 16:09:49 +0900

    regist

--- pages/registration.js
+++ pages/registration.js
@@ -6,11 +6,84 @@ export default function Registration({ Providers }) {
     const [session] = useSession()
     return (
         <Layout title="会員登録">
-            <form className="w-full max-w-lg">
-                <div className="flex flex-wrap -mx-3 mb-6">
-                <input className="block w-full text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:bg-white" id="grid-first-name" type="text" placeholder="Name"/>
-                </div>
-            </form>
+            <div className="bg-gray-100 mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
+    <form>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div className="-mx-3 md:flex mb-6">
+          <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="company">
+              Company Name*
+            </label>
+            <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Tutsplus" />
+            <div>
+              <span className="text-red-500 text-xs italic">
+                Please fill out this field.
+              </span>
+            </div>
+          </div>
+          <div className="md:w-1/2 px-3">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="title">
+              Title*
+            </label>
+            <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="title" type="text" placeholder="Software Engineer" />
+          </div>
+        </div>
+        <div className="-mx-3 md:flex mb-6">
+          <div className="md:w-full px-3">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="application-link">
+              Application Link*
+            </label>
+            <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="text" placeholder="http://...." />
+          </div>
+        </div>
+        <div className="-mx-3 md:flex mb-2">
+          <div className="md:w-1/2 px-3 mb-6 md:mb-0">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="location">
+              Location*
+            </label>
+            <div>
+              <select className="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="location">
+                <option>Abuja</option>
+                <option>Enugu</option>
+                <option>Lagos</option>
+              </select>
+            </div>
+          </div>
+          <div className="md:w-1/2 px-3">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="job-type">
+              Job Type*
+            </label>
+            <div>
+              <select className="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="job-type">
+                <option>Full-Time</option>
+                <option>Part-Time</option>
+                <option>Internship</option>
+              </select>
+            </div>
+          </div>
+          <div className="md:w-1/2 px-3">
+            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="department">
+              Department*
+            </label>
+            <div>
+              <select className="w-full bg-gray-200 border border-gray-200 text-black text-xs py-3 px-4 pr-8 mb-3 rounded" id="department">
+                <option>Engineering</option>
+                <option>Design</option>
+                <option>Customer Support</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="-mx-3 md:flex mt-2">
+          <div className="md:w-full px-3">
+            <button className="md:w-full bg-gray-900 text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full">
+              Button
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
         </Layout>
     )
 }
