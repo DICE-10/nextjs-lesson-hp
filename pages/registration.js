d016487cbaf95b637ba9ddd82673489c5c49dadb
dice <ds_work_prog@outlook.jp>
2021-10-16 13:32:51 +0900

    session

--- pages/registration.js
+++ pages/registration.js
@@ -4,14 +4,13 @@ import { signIn, signOut, useSession,providers } from "next-auth/client";
 
 var fetchForm = "";
 var gitUrl = "";
-var [session] = useSession()
 if (process.browser) {
     fetchForm = document.querySelector('.fetchForm');
     gitUrl = "http://118.27.7.106:9443/api/github";
     // windowやdocumentを使う処理を記述
 }
 export default function Registration({ Providers }) {
-    //const [session] = useSession();
+    const [session] = useSession();
     
 
     return (
@@ -19,15 +18,15 @@ export default function Registration({ Providers }) {
             { console.dir(session) }
             <form action="#" className="fetchForm w-full md:w-3/4 lg:w-3/6 p-4">
                 <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Name" name="userName" value={session?.user?.name} required />
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Name" name="userName" id="user" value={session?.user?.name} required />
                     
                 </div>
                 <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="password" placeholder="password" name="password" required/>
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="password" placeholder="password" id="password" name="password" required/>
                 </div>
                 <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Id" name="userMail" value={session?.user?.email} required />
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="hidden" name="git" value={ session?.accessToken }/>
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Id" name="userMail" id="email" value={session?.user?.email} required />
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="hidden" name="git" id="gitToken" value={ session?.accessToken }/>
                 </div>
                 <div className="p-3">
                     <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="number" placeholder="Mobile Number" required/>
@@ -36,7 +35,7 @@ export default function Registration({ Providers }) {
                     <textarea className="resize-none border rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56" placeholder="Message" required></textarea>
                 </div>
                 <div className="p-3 pt-4">
-                    <button className="w-full bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-4 rounded text-2xl"onClick={() => postFetch()}>
+                    <button className="w-full bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-4 rounded text-2xl"onClick={() => postFetch(session)}>
                     Send
                     </button>
                 </div>
@@ -52,16 +51,24 @@ export async function getStaticProps(context) {
     }
 }
   
-const postFetch = () => {
+const postFetch = (session) => {
     let formData = new FormData(fetchForm);
+    var json = "";
     for (let value of formData.entries()) {
         console.log(value);
     }
-
+    if (session?.user?.name.toLowerCase() == "github") {
+        json = {
+            "name": document.getElementById("name").value,
+            "pass": document.getElementById("password").value,
+            "email": document.getElementById("email").value,
+            "gitToken": document.getElementById("gitToken").value
+        }
+    }
     fetch(gitUrl, {
         mode: 'cors',
         method: 'POST',
-        body: formData
+        body: json
     }).then((response) => {
         if(!response.ok) {
             console.log('error!');
