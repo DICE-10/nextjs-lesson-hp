d5eef7829d05d3b07ce918a66cc171a6a5016d37
dice <ds_work_prog@outlook.jp>
2021-10-23 23:21:37 +0900

    nsv

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
 import Head from "next/head"
 import Link from "next/link"
 import Image from "next/dist/client/image";
+import { signIn, signOut, useSession, providers } from "next-auth/client";
 
 export default function Navbar({ children }) {
+    const [session] = useSession();
     return (
             <div className="fs-header">
                 <div className="fs-hamburger">
@@ -12,8 +14,42 @@ export default function Navbar({ children }) {
                     </label>
                 
                     <div className="fs-navi fs-grid bg-ghostwhite m-grid-2_3 ">
-                        <div></div>
+                    <div></div>
+                    {session && <>
                         <div className="fs-grid m-grid-col-4 fs-nav-menu r-gap ac-center jc-end">
+                            <div className="text-black fs-center">
+                                    <Link href="/">
+                                            <a  className="fs-d-block fs-tlink-center">Home</a>
+                                    </Link>
+                                </div>
+                                <div className="text-black fs-center">
+                                    <Link href="/blog-page">
+                                            <a  className="fs-d-block fs-tlink-center">Blog</a>
+                                    </Link>
+                                </div>
+                                <div className="text-black fs-center">
+                                    <Link href="/contact-page">
+                                            <a  className="fs-d-block fs-tlink-center">Contact</a>
+                                    </Link>
+                                </div>
+                                <div className="text-black fs-center">
+                                    <Link href="/signin">
+                                            <a  className="fs-d-block fs-tlink-center">Sign In</a>
+                                    </Link>
+                            </div>
+                                <div className="text-black fs-center">
+                                        <Link href="/">
+                                                <a  className="fs-d-block fs-tlink-center">ログイン</a>
+                                        </Link>
+                                </div>
+                                <div className="text-black fs-center">
+                                        <Link href="/">
+                                                <a  className="fs-d-block fs-tlink-center">サインアップ</a>
+                                        </Link>
+                                </div>
+                            </div>
+                    </>}
+                        <div className="fs-grid m-grid-col-6 fs-nav-menu r-gap ac-center jc-end">
                             <div className="text-black fs-center">
                                 <Link href="/">
                                         <a  className="fs-d-block fs-tlink-center">Home</a>
@@ -33,6 +69,16 @@ export default function Navbar({ children }) {
                                 <Link href="/signin">
                                         <a  className="fs-d-block fs-tlink-center">Sign In</a>
                                 </Link>
+                        </div>
+                            <div className="text-black fs-center">
+                                    <Link href="/">
+                                            <a  className="fs-d-block fs-tlink-center">ログイン</a>
+                                    </Link>
+                            </div>
+                            <div className="text-black fs-center">
+                                    <Link href="/">
+                                            <a  className="fs-d-block fs-tlink-center">サインアップ</a>
+                                    </Link>
                             </div>
                         </div>
                     </div>
