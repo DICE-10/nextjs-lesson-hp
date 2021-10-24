6d568911a452ce6d223079f36a88e3db9f6c93cc
dice <ds_work_prog@outlook.jp>
2021-10-24 22:51:59 +0900

    css

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,18 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout title="Home">
-        <p className="text-4xl">Welcome to Nextjs</p>
+      <div className="fs-grid m-grid-col-2 fs-wscreen">
+        <div className="fs-outer fs-wfull">
+          <div className="fs-inner fs-aspectHD-hor bg-crimson fs-wfull fs-mx80">
+
+          </div>
+        </div>
+        <div className="fs-outer fs-wfull">
+          <div className="fs-inner fs-aspectHD-vert fs-h450 bg-darkviolet fs-mx60">
+
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
