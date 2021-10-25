ba2bf9a1cababb66f7e4690200064278f09f32a3
dice <ds_work_prog@outlook.jp>
2021-10-25 23:17:47 +0900

    CSS

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
     <Layout title="Home">
       <div className="fs-grid m-grid-col-2 fs-wscreen">
         <div className="fs-outer fs-wfull">
-          <div className="fs-inner fs-aspectHD-hor bg-crimson fs-wfull fs-mx80">
+          <div className="fs-inner fs-aspectHD-hor bg-crimson fs-wfull fs-smx20 fs-mmx30 fs-xlmx40 fs-xxlmx60">
 
           </div>
         </div>
         <div className="fs-outer fs-wfull">
-          <div className="fs-inner fs-aspectHD-vert fs-h450 bg-darkviolet fs-mx60">
+          <div className="fs-inner fs-aspectHD-vert fs-sh400 fs-mh400 fs-h350 bg-darkviolet fs-mx60 fs-my30 fs-mmy40 fs-smy30">
 
           </div>
         </div>
